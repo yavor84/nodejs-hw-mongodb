eb7008771c61dc50fc75ab4ccdfc0192fffd1699
Yavor <pojardim@gmail.com>
2025-05-11 16:37:53 +0300

    add all tasks

---
+++ src/db/initMongoConnection.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+import { getEnvVar } from '../utils/getEnvVar.js';
+
+export const initMongoConnection = async () => {
+  try {
+    const user = getEnvVar('MONGODB_USER');
+    const pwd = getEnvVar('MONGODB_PASSWORD');
+    const url = getEnvVar('MONGODB_URL');
+    const db = getEnvVar('MONGODB_DB');
+    await mongoose.connect(
+      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
+    );
+    console.log('Mongo connection successfully established!');
+  } catch (error) {
+    console.log('Error while setting up mongo connection', error);
+    throw error;
+  }
+};
---
+++ src/server.js
@@ -0,0 +1,58 @@
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+import { getEnvVar } from './utils/getEnvVar.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
+
+const PORT = Number(getEnvVar('PORT', '3000'));
+
+export const setupServer = () => {
+  const app = express();
+  app.use(express.json());
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getAllContacts();
+    res.json({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
+  });
+
+  app.get('/contacts/:contactId', async (req, res, next) => {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+    if (!contact) {
+      res.json({
+        status: 404,
+        message: 'Contact not found',
+      });
+      return;
+    }
+    res.json({
+      status: 200,
+      message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
+    });
+  });
+
+  app.use((req, res, next) => {
+    res.json({
+      status: 404,
+      message: 'Not found',
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
